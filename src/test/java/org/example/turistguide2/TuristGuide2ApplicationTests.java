3cc9b966b1ddcf7f2850dfcaea05de200807dde9
nial0003 <nial0003@stud.kea.dk>
2025-02-17 13:16:44 +0100

    Added skeleton

---
+++ src/test/java/org/example/turistguide2/TuristGuide2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.turistguide2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristGuide2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
