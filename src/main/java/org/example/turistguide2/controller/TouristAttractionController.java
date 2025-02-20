436c112822cdf5c319ccc1f5d2ef05a8814410a6
PSchaufuss <pajo0001@stud.kea.dk>
2025-02-20 11:58:01 +0100

    1) Added method showAddAttractionForm to TouristAttraction. 2) Added method saveAttraction to TouristAttractionController. Also added a formular so you can add an attraction in the templates addAttraction.html.

--- src/main/java/org/example/turistguide2/controller/TouristAttractionController.java
+++ src/main/java/org/example/turistguide2/controller/TouristAttractionController.java
@@ -1,21 +1,42 @@
 package org.example.turistguide2.controller;
 
+import org.example.turistguide2.model.TouristAttraction;
 import org.example.turistguide2.service.TouristAttractionRepoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller("/attractionList")
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/attractions")
 public class TouristAttractionController {
     private final TouristAttractionRepoService touristAttractionRepoService;
 
     @Autowired
-    public TouristAttractionController(TouristAttractionRepoService touristAttractionRepoService){
+    public TouristAttractionController(TouristAttractionRepoService touristAttractionRepoService) {
         this.touristAttractionRepoService = touristAttractionRepoService;
     }
 
-    @GetMapping
-    public String getAttraction(){
+    @GetMapping("/list")
+    public String getAttraction() {
         return "attractionList";
+    }
+
+    @GetMapping("/add")
+    public String showAddAttractionForm() {
+        return "addAttraction";
+    }
+
+    @PostMapping("/save")
+    public String saveAttraction(@RequestParam String name,
+                                 @RequestParam String description,
+                                 @RequestParam String city) {
+        TouristAttraction newAttraction = new TouristAttraction(name, description, city, new ArrayList<>());
+        touristAttractionRepoService.addTouristAttractionToList(newAttraction);
+        return "redirect:/attractions/list";
     }
 }
